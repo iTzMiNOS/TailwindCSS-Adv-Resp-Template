4a7043684f6f77ce116f6dbda7bd4ac1040dd68e
iTzMiNOS <amirerf1998@gmail.com>
2023-06-23 23:52:16 +0300

    new file:   dist/output.css 	new file:   package-lock.json 	new file:   package.json 	new file:   src/index.html 	new file:   src/input.css 	new file:   tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'ssm' : '320px',
+        },
+    },
+  },
+  plugins: [],
+}
+
